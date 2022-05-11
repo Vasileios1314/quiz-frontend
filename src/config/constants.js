a5a76ee56bcb053e8a7d9cbf06f4ed453e5b0531
Vasileios1314 <vasilis.bobonis@hotmail.com>
2022-05-11 19:48:07 +0200

    deploy1

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,7 @@
 export const apiUrtlListPopularMovie =
   "https://api.themoviedb.org/3/movie/popular?api_key=af6f578a62f9646adf2698f4f5cc2853&language=en-US&page=1";
 export const apiUrlPoster = "https://www.themoviedb.org/t/p/w1280";
-// export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const apiKey = "af6f578a62f9646adf2698f4f5cc2853";
-export const apiUrl = "http://localhost:4002";
+// export const apiUrl = "http://localhost:4000";
+export const apiUrl = process.env.REACT_APP_API_URL || "";
+export const DEFAULT_MESSAGE_TIMEOUT = 3000;
